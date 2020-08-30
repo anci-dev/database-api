8684bffea8859ae665df7d6102e7036cb6bfe7ce
walkianm <walkianm@gmail.com>
2020-08-29 21:57:49 -0400

    Changed from mssql to mysql

--- api/sql.js
+++ api/sql.js
@@ -1,27 +1,21 @@
 // Setup and connect to sql server
-var sql = require("mssql");
+var mysql = require("mysql");
 
-var config = {
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    server: process.env.SERVER,
-    database: process.env.DATABASE,
-    port: parseInt(process.env.DB_PORT),
-};
 
-sql.connect(config, function (err) {
-    if (err) {
-        console.log(err);
-        console.log("Unable to connect to SQL database.");
-    }
+var connection = mysql.createConnection({
+  host: process.env.SERVER,
+  port: parseInt(process.env.DB_PORT),
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE
 });
+
+connection.connect();
 
 // For now, it seems like these patterns will be used a lot, so I wrote a couple methods for them
 // Can return to individual sql request handling if need be
 function getData(query, res) {
-    var request = new sql.Request();
-
-    request.query(query, function (err, response) {
+    connection.query(query, function (err, response) {
         if (err) {
             console.log(err);
             res.status(500).json({success: false, error: err});
@@ -33,9 +27,7 @@ function getData(query, res) {
 
 // What kinds of responses are good for posts? Just confirmation?
 function updateData(query, res) {
-    var request = new sql.Request();
-
-    request.query(query, function (err, response) {
+    connection.query(query, function (err, response) {
         if (err) {
             console.log(err);
             res.status(500).json({success: false, error: err});
